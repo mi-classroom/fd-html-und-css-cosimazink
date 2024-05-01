c8bfb4aac61f1aabcf33bda0e4f2fd60f64264e6
cosimazink <cosima_hiromi.zink@smail.th-koeln.de>
2024-05-01 16:36:52 +0200

    javascript - ergänzung der URL-Verlinkung

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,3 +5,15 @@ button.addEventListener("click", () => {
     menu.classList.toggle("is-active");
     button.classList.toggle("is-active");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuLinks = document.querySelectorAll("nav ul li a");
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault(); // Verhindere das Standardverhalten des Links
+            const url = this.getAttribute("href");
+            window.location.href = url; // Weiterleitung zur angegebenen URL
+        });
+    });
+});
